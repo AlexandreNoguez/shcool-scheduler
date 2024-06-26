b466d987d7ac3ad0870a2cd2785b481a120f1bc5
Alexandre Noguez <alexandrenoguez_rs@yahoo.com.br>
2024-06-26 20:54:14 -0300

    feat: added forms to create schedule

--- src/app/components/ScheduleTable.jsx
+++ src/app/components/ScheduleTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 // Definindo os horários dos períodos
 const periods = {
@@ -7,17 +7,8 @@ const periods = {
 };
 
 const daysOfWeek = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
-const rooms = ["Sala 1", "Sala 2", "Sala 3", "Sala 4", "Sala 5"];
-const teachers = {
-  "Português 1": "Português",
-  "Português 2": "Português",
-  "Matemática 1": "Matemática",
-  "Matemática 2": "Matemática",
-  "Geografia 1": "Geografia",
-  "Geografia 2": "Geografia",
-};
 
-const initializeSchedule = () => {
+const initializeSchedule = (rooms) => {
   const schedule = {};
   for (let room of rooms) {
     schedule[room] = {};
@@ -31,7 +22,7 @@ const initializeSchedule = () => {
   return schedule;
 };
 
-const allocateSubject = (schedule, subject) => {
+const allocateSubject = (schedule, teachers, subject) => {
   let subjectTeachers = Object.keys(teachers).filter(
     (teacher) => teachers[teacher] === subject
   );
@@ -42,13 +33,16 @@ const allocateSubject = (schedule, subject) => {
 
     while (periodsPerTeacher > 0 && attempts < 100) {
       let turn = periodsPerTeacher > 2 ? "afternoon" : "morning"; // Primeiro preenche 2 períodos da tarde
-      let room = rooms[Math.floor(Math.random() * rooms.length)];
+      let room =
+        Object.keys(schedule)[
+          Math.floor(Math.random() * Object.keys(schedule).length)
+        ];
       let day = daysOfWeek[Math.floor(Math.random() * daysOfWeek.length)];
       let period = Math.floor(Math.random() * 5);
 
       // Verificar se já existe a mesma aula no mesmo dia e turno
       let periodOccupied = false;
-      for (let r of rooms) {
+      for (let r of Object.keys(schedule)) {
         if (schedule[r][day][turn][period] === teacher) {
           periodOccupied = true;
           break;
@@ -63,28 +57,67 @@ const allocateSubject = (schedule, subject) => {
     }
 
     if (periodsPerTeacher > 0) {
-      console.log(`Não foi possível alocar todos os períodos para ${teacher}`);
+      alert(`Não foi possível alocar todos os períodos para ${teacher}`);
     }
   }
 };
 
-const allocatePeriods = (schedule) => {
-  const subjects = ["Português", "Matemática", "Geografia"];
+const allocatePeriods = (schedule, teachers, subjects) => {
   for (let subject of subjects) {
-    console.log(`Alocando horários para ${subject}`);
-    allocateSubject(schedule, subject);
+    allocateSubject(schedule, teachers, subject);
   }
 };
 
 const ScheduleTable = () => {
-  const [scheduleData, setScheduleData] = useState(initializeSchedule());
+  const [teachers, setTeachers] = useState({});
+  const [rooms, setRooms] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+  const [scheduleData, setScheduleData] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
+  const [inputFields, setInputFields] = useState({
+    teacherName: "",
+    teacherSubject: "",
+    room: "",
+    subject: "",
+  });
+console.log(scheduleData);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputFields({
+      ...inputFields,
+      [name]: value,
+    });
+  };
 
-  useEffect(() => {
-    const schedule = initializeSchedule();
-    allocatePeriods(schedule);
+  const handleAddTeacher = (e) => {
+    e.preventDefault();
+    setTeachers({
+      ...teachers,
+      [inputFields.teacherName]: inputFields.teacherSubject,
+    });
+    setInputFields({ ...inputFields, teacherName: "", teacherSubject: "" });
+  };
+
+  const handleAddRoom = (e) => {
+    e.preventDefault();
+    setRooms([...rooms, inputFields.room]);
+    setInputFields({ ...inputFields, room: "" });
+  };
+
+  const handleAddSubject = (e) => {
+    e.preventDefault();
+    setSubjects([...subjects, inputFields.subject]);
+    setInputFields({ ...inputFields, subject: "" });
+  };
+
+  const handleGenerateSchedule = () => {
+    if (!teachers || !rooms || !subjects) {
+      return alert("Preencha os campos antes de gerar a grade horária");
+    }
+    const schedule = initializeSchedule(rooms);
+    allocatePeriods(schedule, teachers, subjects);
     setScheduleData(schedule);
-  }, []);
+  };
 
   const handleNextPage = () => {
     if (currentPage < rooms.length - 1) {
@@ -102,74 +135,199 @@ const ScheduleTable = () => {
 
   return (
     <div className="p-6">
-      <div className="flex justify-between mb-4">
-        <button
-          onClick={handlePrevPage}
-          disabled={currentPage === 0}
-          className="px-4 py-2 bg-gray-300 rounded"
-        >
-          Anterior
-        </button>
-        <span className="text-xl font-bold">{room}</span>
-        <button
-          onClick={handleNextPage}
-          disabled={currentPage === rooms.length - 1}
-          className="px-4 py-2 bg-gray-300 rounded"
-        >
-          Próximo
-        </button>
-      </div>
-      <div className="mb-8">
-        {daysOfWeek.map((day) => (
-          <div key={day} className="mb-4">
-            <h3 className="text-xl font-semibold mb-2">{day}</h3>
-            <div className="flex flex-wrap -mx-2">
-              <div className="w-full md:w-1/2 px-2">
-                <h4 className="text-lg font-semibold mb-2">Manhã</h4>
-                <table className="min-w-full bg-white border border-gray-200">
-                  <thead>
-                    <tr>
-                      <th className="py-2 px-4 border-b">Horário</th>
-                      <th className="py-2 px-4 border-b">Professor</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {periods.morning.map((period, index) => (
-                      <tr key={index}>
-                        <td className="py-2 px-4 border-b">{period}</td>
-                        <td className="py-2 px-4 border-b">
-                          {scheduleData[room][day].morning[index] || "Livre"}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-              <div className="w-full md:w-1/2 px-2">
-                <h4 className="text-lg font-semibold mb-2">Tarde</h4>
-                <table className="min-w-full bg-white border border-gray-200">
-                  <thead>
-                    <tr>
-                      <th className="py-2 px-4 border-b">Horário</th>
-                      <th className="py-2 px-4 border-b">Professor</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {periods.afternoon.map((period, index) => (
-                      <tr key={index}>
-                        <td className="py-2 px-4 border-b">{period}</td>
-                        <td className="py-2 px-4 border-b">
-                          {scheduleData[room][day].afternoon[index] || "Livre"}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+      <div className="flex">
+        <div className="w-1/2">
+          <form onSubmit={handleAddTeacher} className="mb-4 flex flex-col">
+            <h2 className="text-xl font-bold mb-2">Adicionar Professores</h2>
+            <input
+              type="text"
+              name="teacherName"
+              value={inputFields.teacherName}
+              onChange={handleInputChange}
+              placeholder="Nome do Professor"
+              className="px-4 py-2 border rounded mb-2"
+            />
+            <input
+              type="text"
+              name="teacherSubject"
+              value={inputFields.teacherSubject}
+              onChange={handleInputChange}
+              placeholder="Matéria"
+              className="px-4 py-2 border rounded mb-2"
+            />
+            <div>
+              <button
+                type="button"
+                onClick={handleAddTeacher}
+                className="px-4 py-2 bg-blue-500 text-white rounded mb-4"
+              >
+                Adicionar Professor
+              </button>
             </div>
+          </form>
+
+          <form onSubmit={handleAddRoom} className="mb-4 flex flex-col">
+            <h2 className="text-xl font-bold mb-2">Adicionar Salas</h2>
+            <input
+              type="text"
+              name="room"
+              value={inputFields.room}
+              onChange={handleInputChange}
+              placeholder="Nome da Sala"
+              className="px-4 py-2 border rounded mb-2"
+            />
+            <button
+              type="button"
+              className="px-4 py-2 bg-blue-500 text-white rounded mb-4"
+            >
+              Adicionar Sala
+            </button>
+          </form>
+
+          <form className="mb-4 flex flex-col">
+            <h2 className="text-xl font-bold mb-2">Adicionar Matérias</h2>
+            <input
+              type="text"
+              name="subject"
+              value={inputFields.subject}
+              onChange={handleInputChange}
+              placeholder="Nome da Matéria"
+              className="px-4 py-2 border rounded mb-2"
+            />
+            <button
+              onClick={handleAddSubject}
+              className="px-4 py-2 bg-blue-500 text-white rounded mb-4"
+            >
+              Adicionar Matéria
+            </button>
+          </form>
+
+          <div className="mb-8 flex justify-center items-center">
+            <button
+              onClick={handleGenerateSchedule}
+              className="px-4 py-2 bg-green-500 text-white rounded"
+            >
+              Gerar Cronograma
+            </button>
           </div>
-        ))}
+        </div>
+
+        <div className="w-1/2 pl-4">
+          <div className="mb-4 h-40 overflow-y-scroll">
+            <h2 className="text-xl font-bold mb-2">Professores Adicionados</h2>
+            <ul className="list-disc pl-6">
+              {Object.keys(teachers).map((teacher) => (
+                <li key={teacher}>
+                  {teacher} - {teachers[teacher]}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="mb-4 h-40 overflow-y-scroll">
+            <h2 className="text-xl font-bold mb-2">Salas Adicionadas</h2>
+            <ul className="list-disc pl-6">
+              {rooms.map((room, index) => (
+                <li key={index}>{room}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="mb-4 h-40 overflow-y-scroll">
+            <h2 className="text-xl font-bold mb-2">Matérias Adicionadas</h2>
+            <ul className="list-disc pl-6">
+              {subjects.map((subject, index) => (
+                <li key={index}>{subject}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
+
+      {scheduleData !== null && (
+        <>
+          <div className="flex justify-between mb-4">
+            <button
+              onClick={handlePrevPage}
+              disabled={currentPage === 0}
+              className="px-4 py-2 bg-gray-300 rounded"
+            >
+              Anterior
+            </button>
+            <span className="text-xl font-bold">{room}</span>
+            <button
+              onClick={handleNextPage}
+              disabled={currentPage === rooms.length - 1}
+              className="px-4 py-2 bg-gray-300 rounded"
+            >
+              Próximo
+            </button>
+          </div>
+          <div className="mb-8">
+            {daysOfWeek.map((day) => (
+              <div key={day} className="mb-4">
+                <h3 className="text-xl font-semibold mb-2">{day}</h3>
+                <div className="flex flex-wrap -mx-2">
+                  <div className="w-full md:w-1/2 px-2">
+                    <h4 className="text-lg font-semibold mb-2">Manhã</h4>
+                    <table className="min-w-full  border border-gray-200">
+                      <thead>
+                        <tr>
+                          <th className="py-2 px-4 border-b">Horário</th>
+                          <th className="py-2 px-4 border-b">Professor</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {console.log(periods.morning.length)}
+                        {periods.morning.length > 0 &&
+                          periods.morning.map((period, index) => (
+                            <tr key={index}>
+                              <td className="py-2 px-4 border-b">{period}</td>
+                              <td className="py-2 px-4 border-b">
+                                {/* {console.log(
+                                  scheduleData[room][day]
+                                    ? scheduleData[room][day].morning[index]
+                                    : "",
+                                  "=========",
+                                  period
+                                )} */}
+                                {scheduleData[room][day].morning[index]
+                                  ? scheduleData[room][day].morning[index]
+                                  : "" || "Livre"}
+                              </td>
+                            </tr>
+                          ))}
+                      </tbody>
+                    </table>
+                  </div>
+                  <div className="w-full md:w-1/2 px-2">
+                    <h4 className="text-lg font-semibold mb-2">Tarde</h4>
+                    <table className="min-w-full  border border-gray-200">
+                      <thead>
+                        <tr>
+                          <th className="py-2 px-4 border-b">Horário</th>
+                          <th className="py-2 px-4 border-b">Professor</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {periods.afternoon.map((period, index) => (
+                          <tr key={index}>
+                            <td className="py-2 px-4 border-b">{period}</td>
+                            <td className="py-2 px-4 border-b">
+                              {scheduleData[room][day].afternoon[index] ||
+                                "Livre"}
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-8">Horário das Salas</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center">Gerador de Grade Horária</h1>
       <ScheduleTable />
     </div>
   );
