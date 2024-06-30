35a3a08925548dc63c3ae8b199f3f0de6cbafae2
Alexandre Noguez <alexandrenoguez_rs@yahoo.com.br>
2024-06-29 23:42:55 -0300

    feat: added logo to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import ScheduleTable from "./components/ScheduleTable";
+
+import logo from '@/app/assets/logo-alcebiades.jpg'
 
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-8 text-center">Gerador de Grade Horária</h1>
+      <div className="flex justify-evenly items-center">
+        <Image className="rounded-lg" src={logo} alt="logo da escola" width={100} height={100} />
+        <h1 className="text-3xl font-bold mb-8 text-center">
+          Gerador de Grade Horária
+        </h1>
+      </div>
       <ScheduleTable />
     </div>
   );
